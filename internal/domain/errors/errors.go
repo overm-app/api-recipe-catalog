ab02322b60cd3eb68aae7bc8132dbca676d1b8d4
Mario Irigoyen <marioirigoyen24@outlook.com>
2026-03-05 21:26:38 -0600

    feat: error models

---
+++ internal/domain/errors/errors.go
@@ -0,0 +1,66 @@
+package errors
+
+type AppError struct {
+	Code	ErrorCode
+	Message string
+	HTTPStatus int
+	Err 	 error
+}
+
+func (e *AppError) Error() string {
+	return e.Message
+}
+
+func (e *AppError) Unwrap() error {
+	return e.Err
+}
+
+func Validation(code ErrorCode, message string) *AppError {
+	return &AppError{
+		Code: code,
+		Message: message,
+		HTTPStatus: 400,
+	}
+}
+
+func RecipeForbidden(code ErrorCode, message string) *AppError {
+	return &AppError{
+		Code: code,
+		Message: message,
+		HTTPStatus: 404,
+	}
+}
+
+
+func NotFound(code ErrorCode, message string) *AppError {
+	return &AppError{
+		Code: code,
+		Message: message,
+		HTTPStatus: 404,
+	}
+}
+
+func Unauthorized(code ErrorCode, message string) *AppError {
+	return &AppError{
+		Code: code,
+		Message: message,
+		HTTPStatus: 401,
+	}
+}
+
+func Conflict(code ErrorCode, message string) *AppError {
+	return &AppError{
+		Code: code,
+		Message: message,
+		HTTPStatus: 409,
+	}
+}
+
+func Internal(message string, err error) *AppError {
+	return &AppError{
+		Code: ErrInternal,
+		Message: message,
+		Err: err,
+		HTTPStatus: 500,
+	}
+}
