ec0d5726295f5e845ef54e099eaeb77d010d913a
Mario Irigoyen <89858719+Tar-Mairon24@users.noreply.github.com>
2026-03-12 13:44:50 -0600

    Revert "Issue 1 basic structure and enpoints for recipe crud (#2)" (#4)
    This reverts commit 8824d1e85d8ead1af76da9e095531be6a4088f70.

--- internal/domain/ports/jwt_service.go
+++
@@ -1,7 +0,0 @@
-package ports
-
-import "github.com/overm-app/api-recipe-catalog/internal/domain/models"
-
-type JWTService interface {
-    ValidateToken(tokenString string) (*models.JWTClaims, error)
-}
